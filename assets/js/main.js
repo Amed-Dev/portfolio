5782cf6039cb0e8b3590c4f9d4da7c5a120f3d22
Amed Inti Pereda Gomez <116046824+Amed-Dev@users.noreply.github.com>
2023-11-25 23:29:46 -0500

    v1

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+const sections = document.querySelectorAll(".container .content section");
+const navItems = document.querySelectorAll(".nav-menu a");
+
+const observer = new IntersectionObserver(
+  (entradas, observador) => {
+    entradas.forEach((entrada) => {
+      if (entrada.isIntersecting) {
+        const id = "#" + entrada.target.id;
+        history.pushState({}, entrada.target.innetText, id);
+
+        navItems.forEach((navItem) => {
+          navItem.classList.remove("active");
+
+          const href = navItem.attributes.href.nodeValue;
+          console.log(id);
+          if (href === id) {
+            navItem.classList.add("active");
+          }
+        });
+      }
+    });
+  },
+  {
+    threshold: 1,
+    rootMargin: "0px 0px -2% 0px",
+  }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
